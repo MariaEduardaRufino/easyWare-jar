f327672a64d17c6c688765b7818fc00ac1715378
MariaEduardaRufino <maria.rsilva@sptech.school>
2023-05-15 17:21:53 -0300

    resolvendo probleminhas

--- project-easy/src/main/java/sptech/jswing/models/Conexao2.java
+++ project-easy/src/main/java/sptech/jswing/models/Conexao2.java
@@ -17,7 +17,7 @@ public class Conexao2 {
     public Conexao2() {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://18.206.158.133/banco1");
+        dataSource.setUrl("jdbc:mysql://0.0.0.0/banco1?useSSL=false");
         dataSource.setUsername("root");
         dataSource.setPassword("urubu100");
         this.connection = new JdbcTemplate(dataSource);
